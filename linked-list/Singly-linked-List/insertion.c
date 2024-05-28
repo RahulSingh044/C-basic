32754d2e5d4c20fe1d3b6172b4a6a9750aea08bf
Rahul Singh <156307975+RahulSingh044@users.noreply.github.com>
2024-05-28 23:26:00 +0530

    Update insertion.c

--- linked-list/Singly-linked-List/insertion.c
+++ linked-list/Singly-linked-List/insertion.c
@@ -1,105 +1,113 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-struct Node
-{
+#include<stdio.h>
+#include<stdlib.h>
+struct Node{
     int data;
     struct Node *next;
 };
 
-// struct Node* head = NULL;
+void printlist(struct Node *ptr){
+  while (ptr != NULL)
+  {
+    printf("%d\t",ptr->data);
+    ptr = ptr->next;
+  }
+}
 
-void printList(struct Node *ptr)
-{
-    while (ptr != NULL)
-    {
-        printf("Element: %d \n", ptr->data);
-        ptr = ptr->next;
+struct Node *insertAtBeg(struct Node *head, int value) {
+  struct Node *ptr  = (struct Node*)malloc(sizeof(struct Node));
+  ptr -> data = value;
+  ptr->next = head;
+  return ptr;
+}
+
+struct Node *insertAtEnd(struct Node *head, int value) {
+  struct Node *curr = head;
+  struct Node *ptr  = (struct Node*)malloc(sizeof(struct Node));
+  ptr->data = value;
+  while (curr -> next != NULL)
+  {
+    curr = curr->next;
+  }
+  curr->next = ptr;
+  ptr->next = NULL;
+  return head;
+}
+
+struct Node *insertAfterNode(struct Node *head, int value, struct Node *Pnode) {
+  struct Node *ptr  = (struct Node*)malloc(sizeof(struct Node));
+  ptr->data = value;
+  ptr->next = Pnode->next ;
+  Pnode->next = ptr; 
+  return head;
+}
+
+struct Node *insertBeforeNode(struct Node *head, int value, struct Node *Pnode) {
+  struct Node *curr = head;
+  struct Node *ptr  = (struct Node*)malloc(sizeof(struct Node));
+  ptr->data = value;
+  while (curr -> next != Pnode)
+  {
+    curr = curr->next;
+  }
+  
+  curr->next = ptr;
+  ptr->next = Pnode;
+  return head;
+}
+
+struct Node *insertAtParticularPosition(struct Node *head, int value, int pos) {
+  struct Node *curr = head;
+  struct Node *ptr  = (struct Node*)malloc(sizeof(struct Node));
+  ptr->data = value;
+  int i = 0;
+  while (i != pos)
+  {
+    curr = curr->next;
+    i++;
+  }
+  ptr ->next = curr->next;
+  curr->next = ptr;
+  return head;
+}
+
+void *SearchNode(struct Node *head, int value) {
+  struct Node *curr = head;
+  int i = 0;
+  while (curr != NULL )
+  {
+    curr = curr->next;
+    i++;
+    if(curr->data == value){
+      printf("%d is found at index: %d",value,i);
     }
-}
+  }
+  printf("The data is not found");
+} 
 
-// Time commplexity of this function is O(1)
-struct Node *insertAtBeginning(struct Node *head, int value)
-{
-    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
-    ptr->next = head;
-    ptr->data = value;
-    return ptr;
-}
+int main() {
+  struct Node *head;
+  struct Node *first;
+  struct Node *second;
 
-// Time commplexity of this function is O(n)
-struct Node *inBetweenNodes(struct Node *head, int value, int indx)
-{
-    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
-    struct Node *p = head;
-    int i = 0;
 
-    while (i != indx - 1)
-    {
-        p = p->next;
-        i++;
-    }
+  head = (struct Node*)malloc(sizeof(struct Node));
+  first = (struct Node*)malloc(sizeof(struct Node));
+  second = (struct Node*)malloc(sizeof(struct Node));
+  
+  head->data = 7;
+  head->next = first;
+  first->data = 9;
+  first->next = second;
+  second->data = 2;
+  second->next = NULL;
 
-    ptr->data = value;
-    ptr->next = p->next;
-    p->next = ptr;
-    return head;
-}
-
-// Time commplexity of this function is O(n)
-struct Node *insertAtEnd(struct Node *head, int value)
-{
-    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
-    struct Node *p = head;
-    ptr->data = value;
-
-    while (p->next != NULL)
-    {
-        p = p->next;
-    }
-    p->next = ptr;
-    ptr->next = NULL;
-    return head;
-}
-
-// Time commplexity of this function is O(1)
-struct Node *insertAfterNode(struct Node *head, struct Node *prevNode, int value)
-{
-    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
-    ptr -> data = value;
-    ptr -> next = prevNode -> next;
-    prevNode -> next = ptr;
-    return head;
-}
-
-int main()
-{
-    struct Node *head;
-    struct Node *second;
-    struct Node *third;
-    struct Node *fourth;
-
-    // Allocate memory for nodes in linked list
-    head = (struct Node *)malloc(sizeof(struct Node));
-    second = (struct Node *)malloc(sizeof(struct Node));
-    third = (struct Node *)malloc(sizeof(struct Node));
-    fourth = (struct Node *)malloc(sizeof(struct Node));
-
-    head->data = 12;
-    head->next = second;
-    second->data = 20;
-    second->next = third;
-    third->data = 95;
-    third->next = fourth;
-    fourth->data = 15;
-    fourth->next = NULL;
-
-    printList(head);
-    // head = insertAtBeginning(head, 45);
-    // inBetweenNodes(head, 106, 2);
-    // insertAtEnd(head, 492);
-    insertAfterNode(head,third,654);
-    printf("\n");
-    printList(head);
-    return 0;
+ printlist(head);
+//  head = insertAtBeg(head,96);
+// insertAtEnd(head,36);
+// insertAfterNode(head,75,first);
+// insertBeforeNode(head,88,second);
+// insertAtParticularPosition(head,55,2);
+// SearchNode(head,9);
+//  printf("\n");
+//  printlist(head);
 }
